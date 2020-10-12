82337ab99c129e84f6f7efbd4ca80b679f837de5
SIsilicon <sisilicon28@gmail.com>
2020-10-12 18:46:47 -0500

    Implemented pooled LOD updating via LODManager. Improved Node UI.

---
+++ addons/lod/lod_manager.gd
@@ -0,0 +1,48 @@
+# Copyright © 2020 Hugo Locurcio and contributors - MIT License
+# See `LICENSE.md` included in the source distribution for details.
+extends Node
+
+# Registered lod objects.
+var lod_objects := []
+
+# Index in the lod array that was last processed.
+var current_idx := 0
+
+# Number of lods processed in the current frame. Access this for debugging.
+var lods_processed := 0
+
+# How much time can be used to process lods in milliseconds.
+# If not all the lods can be processed within this time, they'll be processed in the next frame.
+var refresh_threshold_ms := 10
+
+
+func _ready() -> void:
+	if ProjectSettings.has_setting("lod/refresh_threshold_ms"):
+		refresh_threshold_ms = ProjectSettings.get_setting("lod/refresh_threshold_ms")
+
+
+func _process(_delta: float) -> void:
+	if current_idx >= lod_objects.size():
+		current_idx = 0
+
+	lods_processed = 0
+	var prev_idx := current_idx
+	var time := OS.get_ticks_msec()
+
+	while OS.get_ticks_msec() - time < refresh_threshold_ms:
+		lod_objects[current_idx].update_lod()
+		lods_processed += 1
+
+		current_idx = wrapi(current_idx + 1, 0, lod_objects.size())
+		if prev_idx == current_idx:
+			break
+
+# Register lod object, if it's not already in the list.
+func register_lod_object(object: Spatial) -> void:
+	if not lod_objects.has(object):
+		lod_objects.append(object)
+
+# Unregister lod object, if it's still in the list.
+func unregister_lod_object(object: Spatial) -> void:
+	if lod_objects.has(object):
+		lod_objects.erase(object)
